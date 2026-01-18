346b4bcc250aab8688d5f061c45dfeca1b76eaa5
root <root@66-116-227-112.webhostbox.net>
2026-01-18 06:44:01 -0700

    config

--- project/project/settings.py
+++ project/project/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bol#g^_22bu(7822)2jm9us7d+=k1xjocov#g!no4s_t^r!t0n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -123,9 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-
+STATIC_ROOT = '/var/www/kuvamprotosolutions.in/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
